82d72b471387a121a2a1a124ee99e5711a5c589d
ivgtr <ivgtr.me@gmail.com>
2023-12-28 22:29:28 +0900

    meta周り調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 import { CreditsCanvas } from "@/components/CreaditsCanvas";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansJP400 = Noto_Sans_JP({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
-      <CreditsCanvas />
-    </main>
+    <>
+      <Head>
+        <title>無限エンド・クレジット</title>
+      </Head>
+
+      <main className={`flex min-h-screen flex-col items-center justify-between ${notoSansJP400.className}`}>
+        <CreditsCanvas />
+      </main>
+    </>
   );
 }
